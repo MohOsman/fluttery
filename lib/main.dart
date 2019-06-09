aae27109504eb7e83c515894b9e5f43619b81b7b
mohamed osman <mohamedosman@mohameds-MacBook-Pro.local>
2019-06-09 21:41:27 +0200

    changed some small stuff

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new Center(
-      child: new MyApp();
+      child: new MyApp()
     ),
   );
 }
@@ -13,7 +13,16 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return new MaterialApp(
       title: "hello flutter",
-      home: new Scaffold(),
+      home: new Scaffold(
+        appBar:  new  AppBar(title: new Text("fluttery"),
+        ),
+        body: new Center(
+          child: new RaisedButton(
+            onPressed: null,
+            child: new Text("my button") ,
+          ),
+        ),
+      ),
     );
   }
 }
